95ed62b89e366e354343edf09d942e32f93aa1db
mark oberhaus <moberhau@studentloan.org>
2014-02-24 09:31:13 -0600

    Adding Client side Validation for new appointments(WIP)

--- scheduleit/scheduleit/js/appointments.js
+++ scheduleit/scheduleit/js/appointments.js
@@ -1,16 +1,18 @@
+var isValidNewAppointment = false;
+
 function buildAppointment(name, email, phone, time, amPm) {
     var appointment = $('<div class="appointment"></div>');
 
     appointment.append('<div class="appointment-overview"></div>');
     appointment.find('.appointment-overview').append('<img class="remove-control" src="images/remove.png"/>');
     appointment.find('.appointment-overview').append('<img class=" dropdown-control" src="images/arrowheadLeft.png"/>');
-    appointment.find('.appointment-overview').append('<p>'+time + amPm+' '+name+'</p>');
+    appointment.find('.appointment-overview').append('<p>' + time + amPm + ' ' + name + '</p>');
 
     appointment.append('<div class="appointment-details"></div>');
     appointment.find('.appointment-details').append('<ul></ul>');
-    appointment.find('.appointment-details ul').append('<li>Full Name:'+ name +'</li>');
-    appointment.find('.appointment-details ul').append('<li>Phone Number:'+ phone +'</li>');
-    appointment.find('.appointment-details ul').append('<li>Email Address:'+ email +'</li>');
+    appointment.find('.appointment-details ul').append('<li>Full Name:' + name + '</li>');
+    appointment.find('.appointment-details ul').append('<li>Phone Number:' + phone + '</li>');
+    appointment.find('.appointment-details ul').append('<li>Email Address:' + email + '</li>');
 
     return appointment;
 }
@@ -50,11 +52,7 @@ $("#appointments").on('click', '.remove-control', function () {
 });
 
 $(".page-controls").on('click', '.remove-new-control', function () {
-    var conf = confirm("You are about to cancel this appointment. \n Proceed? ");
-
-    if (conf == true) {
-        fadeOutAppointmentForm($(this));
-    }
+    fadeOutAppointmentForm($(this));
 });
 
 $(".add-appointment").on('click', function (event) {
@@ -64,15 +62,53 @@ $(".add-appointment").on('click', function (event) {
 
 $('.appointment-form').on('click', '.add-it', function (event) {
     event.preventDefault();
-    $form = $(this).closest('.appointment-form');
 
-    var name = $form.find('#new-appointment-name').val();
-    var email = $form.find('#new-appointment-email').val();
-    var phone = $form.find('#new-appointment-phone').val();
-    var time = $form.find('#new-appointment-time').val();
-    var amPm = $form.find('#new-appointment-am-pm').val();
+    if (isValidNewAppointment) {
+        $form = $(this).closest('.appointment-form');
 
-    $('#appointments').append(buildAppointment(name, email, phone, time, amPm));
+        var name = $form.find('#new-appointment-name').val();
+        var email = $form.find('#new-appointment-email').val();
+        var phone = $form.find('#new-appointment-phone').val();
+        var time = $form.find('#new-appointment-time').val();
+        var amPm = $form.find('#new-appointment-am-pm').val();
 
-    fadeOutAppointmentForm($(this));
+        $('#appointments').append(buildAppointment(name, email, phone, time, amPm));
+
+        fadeOutAppointmentForm($(this));
+    }
+});
+
+// Automatic Validation
+function appendErrorMessage(message) {
+    $(".appointment-form h4").append('<p class="error">' + message + '</p>');
+}
+
+function removeErrorMessage() {
+    $(".appointment-form .error").remove();
+}
+
+function addErrorsForInput(element, message) {
+    message = typeof message !== 'undefined' ? message : null;
+    if (message != null) {
+        appendErrorMessage(message);
+    }
+    $(element).addClass('error-input');
+}
+
+$('.appointment-form').on('focus', '#new-appointment-name', function () {
+    addErrorsForInput($(this));
+    isValidNewAppointment = true;
+});
+
+$('.appointment-form').on('change', '#new-appointment-name', function () {
+    var name = $(this).val().trim();
+    if (name == undefined || name == null || name == "") {
+        addErrorsForInput($(this), "Whoa! I think it's safe to assume your client has a name?");
+        isValidNewAppointment = false;
+        console.log("Error Occurred");
+    } else {
+        removeErrorMessage();
+        isValidNewAppointment = true;
+        $(this).removeClass('error-input');
+    }
 });
